03f9f0149e3580bd214378375a1ff3d457715988
Blasszauer Viktor <blasszauer.viktor@nexius.hu>
2022-12-10 12:31:43 +0100

    Add VatRegistrationServiceTests

---
+++ Taxually.TechnicalTest/Taxually.TechnicalTest/UnitTests/VatRegistrationServiceTests.cs
@@ -0,0 +1,51 @@
+﻿using Taxually.TechnicalTest.BusinessLogic;
+using Taxually.TechnicalTest.Models;
+using Xunit;
+using FluentAssertions;
+
+namespace Taxually.TechnicalTest.UnitTests
+{
+    public class VatRegistrationServiceTests
+    {
+        private readonly VatRegistrationRequest _request;
+        private readonly VatRegistrationService _sut;
+
+        public VatRegistrationServiceTests()
+        {
+            _request = new()
+            {
+                CompanyId = "1",
+                CompanyName = "Taxually",
+            };
+        }
+
+        [Theory]
+        [InlineData("GB")]
+        [InlineData("FR")]
+        [InlineData("DE")]
+        public async Task Register__with_known_country_runs_properly(string country)
+        {
+            // Arrange
+            _request.Country = country;
+
+            // Act
+            Action act = async () => await _sut.Register(_request);
+
+            // Assert
+            act.Should().NotThrow<Exception>();
+        }
+
+        [Fact]
+        public async Task Register_with_unknown_country_throws_exception()
+        {
+            // Arrange
+            _request.Country = "NO";
+
+            // Act
+            Action act = async () => await _sut.Register(_request);
+
+            // Assert
+            act.Should().ThrowExactly<Exception>();
+        }
+    }
+}
